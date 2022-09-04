48f4b4afcbf3530d87ce40f0a72763139292d3c0
GraySteamer <44991545+GrAYAtMeRzOOm@users.noreply.github.com>
2022-09-04 14:53:35 +0530

    commons files library added

---
+++ TutionTribe-commons/src/com/gray/tutiontribe/controller/SampleRegisterRemote.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/SessionRemote.java to edit this template
+ */
+package com.gray.tutiontribe.controller;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author grays
+ */
+@Remote
+public interface SampleRegisterRemote {
+
+    void registerClient();
+    
+}
